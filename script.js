6f728275fc3a863c43948df73b998267068e30b2
yacine bouziane <yybouz1802@gmail.com>
2023-09-18 22:45:44 +0100

    desktop version

---
+++ script.js
@@ -0,0 +1,91 @@
+const handOptions = {
+  rock: "/images/Rock.png",
+  paper: "/images/Paper.png",
+  scissor: "/images/Scissors.png",
+};
+let SCORE = 0;
+
+const pickUserHand = (Hand) => {
+  let hands = document.querySelector(".hands");
+  hands.style.display = "none";
+
+  let result = document.querySelector(".result");
+  result.style.display = "flex";
+
+  document.getElementById("userPick").src = handOptions[Hand];
+  pickComputerHand(Hand);
+};
+
+const pickComputerHand = (Hand) => {
+  let hands = ["rock", "paper", "scissor"];
+  let pcHand = hands[Math.floor(Math.random() * 3)];
+
+  document.getElementById("computerPick").src = handOptions[pcHand];
+  referee(Hand, pcHand);
+};
+const referee = (userHand, pcHand) => {
+  let userWins = false;
+  if (userHand == "paper" && pcHand == "scissor") {
+    setDecision("YOU LOSE!");
+  }
+  if (userHand == "paper" && pcHand == "rock") {
+    setDecision("YOU WIN!");
+    setScore(SCORE + 1);
+    userWins = true;
+  }
+  if (userHand == "paper" && pcHand == "paper") {
+    setDecision("It's a tie!");
+  }
+  if (userHand == "rock" && pcHand == "scissor") {
+    setDecision("YOU WIN!");
+    setScore(SCORE + 1);
+    userWins = true;
+  }
+  if (userHand == "rock" && pcHand == "paper") {
+    setDecision("YOU LOSE!");
+  }
+  if (userHand == "rock" && pcHand == "rock") {
+    setDecision("It's a tie!");
+  }
+  if (userHand == "scissor" && pcHand == "scissor") {
+    setDecision("It's a tie!");
+  }
+  if (userHand == "scissor" && pcHand == "rock") {
+    setDecision("YOU LOSE!");
+  }
+  if (userHand == "scissor" && pcHand == "paper") {
+    setDecision("YOU WIN!");
+    setScore(SCORE + 1);
+    userWins = true;
+  }
+  handleWinnerClass(userWins);
+};
+
+const restartGame = () => {
+  let result = document.querySelector(".result");
+  result.style.display = "none";
+
+  let hands = document.querySelector(".hands");
+  hands.style.display = "flex";
+};
+
+const setDecision = (decision) => {
+  document.querySelector(".decision h1").innerText = decision;
+};
+
+const setScore = (newScore) => {
+  SCORE = newScore;
+  document.querySelector(".score h1").innerText = newScore;
+};
+const handleWinnerClass = (userWins) => {
+  const userImg = document.getElementById("userPick");
+  const computerImg = document.getElementById("computerPick");
+
+  if (userWins) {
+    userImg.classList.add("winner");
+    computerImg.classList.remove("winner");
+  } else {
+    userImg.classList.remove("winner");
+    computerImg.classList.add("winner");
+  }
+};
